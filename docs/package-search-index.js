67fc69eb08039d984d297c9e84f9c56cb68d9dff
Jason J. W <71363607+digaji@users.noreply.github.com>
2021-05-22 01:04:37 +0700

    Updated javadocs

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.crudeemail","l":"org.crudeemail"},{"m":"org.crudeemail","l":"org.crudeemail.controller"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.crudeemail","l":"org.crudeemail"},{"m":"org.crudeemail","l":"org.crudeemail.controller"},{"m":"org.crudeemail","l":"org.crudeemail.provider"}];updateSearchResults();
